20adfe29ea6ebbeda6cbf7a2e153a2b9b8f707ad
artfisica <arturos@domoart.ch>
2024-06-05 17:18:42 -0400

    adding k8s

---
+++ ejercicio-kubernetes/terraform/outputs.tf
@@ -0,0 +1,31 @@
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "client_certificate" {
+  value = google_container_cluster.primary.master_auth.0.client_certificate
+}
+
+output "client_key" {
+  value = google_container_cluster.primary.master_auth.0.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
+
+output "network_name" {
+  value = google_compute_network.vpc_network.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.subnetwork.name
+}
+
+output "subnet_cidr" {
+  value = google_compute_subnetwork.subnetwork.ip_cidr_range
+}
